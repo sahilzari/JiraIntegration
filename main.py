5ba2ba3b83468a8909f9d9a6d57ac4007b1cca15
zaris <zarisahil5396@gmail.com>
2025-11-24 16:46:23 +0530

    up

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    input("Preen Any key to Exit ...")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
